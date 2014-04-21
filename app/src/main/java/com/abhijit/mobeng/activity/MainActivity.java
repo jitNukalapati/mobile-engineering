d9355acb676aa342a6c4da76cde47f3db229352f
Abhijit Nukalapati <abhijit@brandingbrand.com>
2014-04-20 20:47:54 -0400

    removing irrelevant boiler plate code

--- app/src/main/java/com/abhijit/mobeng/activity/MainActivity.java
+++ app/src/main/java/com/abhijit/mobeng/activity/MainActivity.java
@@ -4,8 +4,6 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -45,26 +43,6 @@ public class MainActivity extends ActionBarActivity {
         setupListView();
 
         dispatchVolleyRequest(getString(R.string.feed_url)); // fetch data
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 
     /**
